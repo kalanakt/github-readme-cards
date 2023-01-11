e030311b9c88bcb96068447f342b1516c95c35aa
kalanakt <e19198@eng.pdn.ac.lk>
2023-01-11 13:42:31 +0530

    v1.0.0.000

--- src/routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-const router = express.Router()
-
-import profileRoute from './routes/profile-route'
-
-// Profile widget
-router.use('/profile', profileRoute)
-
-export default router
