85aae91bda7ccb80b9fc6639ce7d94ff7babca10
kalanakt <e19198@eng.pdn.ac.lk>
2023-01-24 07:25:42 +0530

    update repo card svg

--- api/repo-card.js
+++ api/repo-card.js
@@ -6,6 +6,7 @@ const app = express();
 const router = express.Router()
 
 const svgrepoCard = require('../templates/repo-card')
+const language_colors = require('../data/language_colors')
 
 // middleware that is specific to this router
 router.use((req, res, next) => {
@@ -14,24 +15,31 @@ router.use((req, res, next) => {
 })
 
 router.get("/", async (req, res) => {
-const { name, repo, theme } = req.query;
+  const { name, repo, theme } = req.query;
   try {
     const response = await axios.get(`https://api.github.com/repos/${name}/${repo}`);
-    const pulls = await axios.get(`https://api.github.com/repos/${name}/${repo}/pulls?state=open`);
+    //const pulls = await axios.get(`https://api.github.com/repos/${name}/${repo}/pulls?state=open`);
 
     const title = response.data.full_name;
+    let description = response.data.description;
+    const language = response.data.language;
     const starCount = response.data.stargazers_count;
     const forksCount = response.data.forks_count;
     const subscribersCount = response.data.subscribers_count;
     const issuesCount = response.data.open_issues_count;
-    const pullsCount = pulls.data.length;
+    //const pullsCount = pulls.data.length;
     // Read SVG template
-    const svg = svgrepoCard.replace("{issuesCount}", issuesCount).replace("{starCount}", starCount).replace("{subscribersCount}", subscribersCount).replace("{pullsCount}", pullsCount).replace("{forksCount}", forksCount).replace("{title}", title);
+    if (description.length > 55) {
+      description = description.slice(0, 50) + '  ...'
+    }
+    const lang_color = language_colors[language]
+    const svg = svgrepoCard.replace("{issuesCount}", issuesCount).replace("{starCount}", starCount).replace("{lang_color}", lang_color).replace("{language}", language).replace("{subscribersCount}", subscribersCount).replace("{forksCount}", forksCount).replace("{description}", description).replace("{title}", title);
     res.setHeader('Content-Type', 'image/svg+xml');
     res.send(svg)
-    
+
   } catch (error) {
     res.send(`Error: ${error.message}`);
-  }});
+  }
+});
 
 module.exports = router
--- templates/repo-card.js
+++ templates/repo-card.js
@@ -1,54 +1,97 @@
 const svgrepoCard = `
-<svg version="1.1" id="Layer_1" width="520" height="200" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 382 210" style="enable-background:new 0 0 382 210" xml:space="preserve">
-    <style>
-        .st1{fill:#a8fdf6}.st3{font-size:18px}.st4{font-family:&apos;SegoeUI&apos;}.st5{font-size:25px}.st6{font-size:12px}.st7{fill:#43a047}.st9{display:none}.st12{fill-rule:evenodd;clip-rule:evenodd;fill:#e91e63}
-    </style>
-    <g>
-        <path id="Card-2" d="M20.7 9h340.6c6.5 0 11.7 5.2 11.7 11.7v168.6c0 6.5-5.2 11.7-11.7 11.7H20.7C14.2 201 9 195.8 9 189.3V20.7C9 14.2 14.2 9 20.7 9z" style="fill:#141321"/>
+
+<svg width="500px" height="150" x="0" y="0" id="github-repo-card" viewBox="0 0 400 120" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" aria-labelledby="descId">
+  <style>
+    .github-repo-card {
+      width: 100%;
+      height: 100%;
+      cursor: pointer;
+    }
+    .header {
+      font: 400 18px &apos;Segoe UI&apos;, Ubuntu, Sans-Serif;
+      fill: #fff;
+    }
+
+    @supports(-moz-appearance: auto) {
+
+      /* Selector detects Firefox */
+      .header {
+        font-size: 15.5px;
+      }
+    }
+
+    .description {
+      font: 400 13px &apos;Segoe UI&apos;, Ubuntu, Sans-Serif;
+      fill: #9f9f9f;
+    }
+
+    .gray {
+      font: 400 12px &apos;Segoe UI&apos;, Ubuntu, Sans-Serif;
+      fill: #9f9f9f
+    }
+
+    .icon {
+      fill: #f9f9f9
+    }
+
+    .icon:hover{
+      fill: #3f2f6f
+    }
+
+    .badge {
+      font: 600 11px &apos;Segoe UI&apos;, Ubuntu, Sans-Serif;
+    }
+  </style>
+  <defs>
+    <linearGradient id="myGradient" gradientTransform="rotate(0)">
+      <stop offset="5%" stop-color="#243949"/>
+      <stop offset="95%" stop-color="##517fa4"/>
+    </linearGradient>
+  </defs>
+  <rect class="github-repo-card" data-testid="card-bg" x="0" y="0" rx="4.5" stroke="#e4e2e2" fill="url('#myGradient')" stroke-opacity="1"/>
+  <g class="github-repo-card" data-testid="card-title" transform="translate(25, 35)">
+    <g transform="translate(0, 0)">
+      <svg class="icon" x="0" y="-13" viewBox="0 0 16 16" version="1.1" width="16" height="16">
+        <path fill-rule="evenodd" d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"/>
+      </svg>
     </g>
-    <text id="key_0" transform="translate(80.932 43.968)" class="st1 st3" style="font-family:'SegoeUI-Bold'">{title}</text>
-    <text transform="translate(252.39 120.969)" class="st1 st4 st5">{forksCount}</text>
-    <text transform="translate(112.745 170.26)" class="st1 st4 st6">{pullsCount}</text>
-    <text transform="translate(346.156 170.066)" class="st1 st4 st6">{subscribersCount}</text>
-    <text transform="translate(79.514 120.968)" class="st1 st4 st5">{starCount}</text>
-    <text transform="translate(219.883 170.544)" class="st1 st4 st6">{issuesCount}</text>
-    <path class="st7" d="M20.9 169.6c.9-.1.9-1.2 0-1.2-1 0-1 1.1 0 1.2zm0 .5c-1.7-.1-1.7-2.2 0-2.3 1.7 0 1.8 2.2 0 2.3z"/>
-    <g id="p1">
-        <path class="st7" d="M20.9 161.1c.9-.1.9-1.2 0-1.2-1 0-1 1.2 0 1.2zm0 .5c-1.7-.1-1.7-2.2 0-2.3 1.7.1 1.8 2.3 0 2.3z"/>
-        <path class="st7" d="M20.5 168v-6.2c0-.2.2-.5.4-.5s.5.2.5.5v6.2c-.1.5-.9.5-.9 0z"/>
+    <g transform="translate(25, 0)">
+      <text x="0" y="0" class="header" data-testid="header">{title}</text>
     </g>
-    <path class="st7" d="M31.2 169.6c.9-.1.9-1.2 0-1.2s-.9 1.2 0 1.2zm0 .5c-1.7-.1-1.7-2.2 0-2.3 1.8.1 1.8 2.3 0 2.3z"/>
-    <g id="p2">
-        <path class="st7" d="M30.8 166v-3.5c-.1-1.1-1-2.1-2.1-1.9h-.9c-.5 0-.5-.8 0-.8h.9c2.6-.1 3.1 2.3 3 4.4v1.7c0 .7-.9.7-.9.1z"/>
-        <path class="st7" d="m27.4 159.1-1.4 1.2c.1 0 .1-.3 0-.3l.5.4.9.9c.5.5-.2 1.3-.7.8l-1-.8c-.3-.3-.8-.6-.8-1.1 0-.3.2-.6.4-.7l1.4-1.3c.5-.6 1.3.3.7.9z"/>
-    </g>
-    <text transform="translate(36.24 170.119)" class="st7 st4 st6">Pull Requests</text>
-    <path class="st1" d="M59.3 77.8c-.1.2 0 .5.2.6l3.4 2.5-1.1 4.3c-.1.3.2.7.5.8.2.1.4 0 .6-.1l3.7-2.4 3.7 2.4c.3.2.7.2 1-.2.1-.2.2-.3.1-.5l-1.1-4.3 3.4-2.5c.3-.2.3-.6.1-.9-.1-.2-.2-.2-.4-.2l-4-.5-2.1-3.7c-.2-.3-.7-.4-1-.2l-.2.2-2.1 3.7-4.1.5c-.3.1-.5.3-.6.5zm5.2.2c.2 0 .4-.2.5-.3l1.6-2.9 1.6 2.9c.1.2.3.3.5.3l2.9.4-2.6 1.8c-.2.2-.3.4-.2.6l.7 3-2.6-1.7c-.2-.2-.6-.2-.8 0l-2.6 1.7.7-3c.1-.2 0-.5-.2-.6l-2.5-1.9 3-.3z" id="p4"/>
-    <text transform="matrix(1.0234 0 0 1 67.814 86.003)" class="st1 st4" style="font-size:16.2601px">   </text>
-    <text transform="matrix(1.0234 0 0 1 81.528 86.003)" class="st1 st4 st3">Stars</text>
-    <g id="p6">
-        <g id="p7">
-            <g id="p7" transform="translate(42.667 42.667)">
-                <path id="p8" d="M129.5 118.5c2.8 0 5 2.2 5 4.9s-2.2 4.9-5 4.9-5-2.2-5-4.9c0-2.8 2.2-4.9 5-4.9zm0 .9c-2.2 0-4 1.8-4 3.9 0 2.2 1.8 3.9 4 3.9s4-1.8 4-3.9c0-2.1-1.8-3.9-4-3.9zm0 1.5c1.4 0 2.5 1.1 2.5 2.4 0 1.3-1.1 2.4-2.5 2.4s-2.5-1.1-2.5-2.4c0-1.3 1.1-2.4 2.5-2.4z" style="fill-rule:evenodd;clip-rule:evenodd;fill:#fb8c00"/>
-            </g>
+  </g>
+  <g class="github-repo-card" data-testid="main-card-body" transform="translate(0, 55)">
+    <text class="description" x="25" y="-5">
+      <tspan dy="1.2em" x="25">{description}</tspan>
+    </text>
+    <g transform="translate(30, 45)">
+      <g transform="translate(0, 0)">
+        <g data-testid="primary-lang">
+          <circle data-testid="lang-color" cx="0" cy="-5" r="6" fill="{lang_color}"/>
+          <text data-testid="lang-name" class="gray" x="15">{language}</text>
         </g>
-    </g>
-    <text transform="translate(183.487 169.619)" class="st4 st6" style="fill:#fb8c00">Issues</text>
-    <g id="p9">
-        <g id="p10">
-            <g id="p11">
-                <path class="st12" d="M277.7 161.8c2.7 0 4.9 1.9 6.3 3.7.2.3.3.7 0 1.1-.1.2-.3.5-.5.7-.2.2-.4.5-.6.7-.1.2-.3.3-.5.5-1.4 1.4-3.5 2.4-5.7 2.1-2.3-.3-4.1-1.9-5.4-3.7-.2-.3-.2-.7 0-1 .1-.2.3-.5.4-.7l.6-.6c.1-.2.3-.3.5-.5 1.3-1.3 2.9-2.3 4.9-2.3zm0 1c-1.6 0-3 .9-4.1 1.9l-.4.4c-.2.2-.3.4-.5.6-.3.4-.3.8 0 1.2 1.1 1.3 2.5 2.5 4.2 2.7 1.8.3 3.5-.6 4.7-1.8l.4-.4c.2-.2.4-.4.5-.6.3-.4.3-.8 0-1.2-1-1.5-2.8-2.8-4.8-2.8z"/>
-                <path class="st12" d="M277.6 164.2c1.2 0 2.3.8 2.3 1.9 0 1.1-1 2-2.2 2-1.2 0-2.3-.8-2.3-1.9 0-1.1 1-1.9 2.2-2zm0 1c.6 0 1.1.4 1.1.9s-.5 1-1.1 1c-.6 0-1.1-.4-1.1-.9s.5-1 1.1-1z"/>
-            </g>
+      </g>
+      <g transform="translate(80.93125, 0)">
+        <g transform="translate(0, 0)">
+          <svg class="icon" y="-12" viewBox="0 0 16 16" version="1.1" width="16" height="16">
+            <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"/>
+          </svg>
         </g>
+        <g transform="translate(20, 0)">
+          <text data-testid="stargazers" class="gray">{starCount}</text>
+        </g>
+      </g>
+      <g transform="translate(141.24375, 0)">
+        <g transform="translate(0, 0)">
+          <svg class="icon" y="-12" viewBox="0 0 16 16" version="1.1" width="16" height="16">
+            <path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"/>
+          </svg>
+        </g>
+        <g transform="translate(20, 0)">
+          <text data-testid="forkcount" class="gray">{forksCount}</text>
+        </g>
+      </g>
     </g>
-    <text transform="translate(289.196 170.26)" class="st4 st6" style="fill:#e91e63">Watchers</text>
-    <g id="p12">
-        <path class="st1" d="M248.6 87.3c.3 0 .5.1.8.2.2.1.6.4.6.8s-.3.7-.6.8c-.3.1-.5.1-.8.1-.3 0-.5 0-.8-.1-.2-.1-.5-.3-.6-.7 0-.4.3-.7.5-.8.3-.2.6-.3.9-.3zm0 .6c-.4 0-.8.3-.7.4-.1.2.3.5.7.5.2 0 .4-.1.5-.2.1-.1.2-.2.2-.3 0-.1 0-.2-.2-.3-.1-.1-.3-.1-.5-.1zM249 84.2c.1.7 0 1.6 0 2.3 0 .6-.9.6-.9 0v-2.3c.1-.5.9-.5.9 0z"/>
-        <path class="st1" d="M244.2 79.7c.1 4.6 8.7 4.7 8.8 0 0-.6.9-.6.9 0 0 5.9-10.7 5.9-10.6 0 0-.6.9-.6.9 0z"/>
-    </g>
-    <path class="st1" d="M243.7 76.6c.3 0 .5.1.8.2.2.1.6.4.6.8s-.3.7-.6.8c-.3.1-.5.1-.8.1-.3 0-.5 0-.8-.1-.2-.1-.5-.3-.6-.7 0-.4.3-.7.5-.8.4-.2.7-.3.9-.3zm0 .6c-.4 0-.8.3-.7.4-.1.2.3.5.7.5.2 0 .4-.1.5-.2.1-.1.2-.2.2-.3 0-.1 0-.2-.2-.3-.1-.1-.3-.1-.5-.1zM253.4 76.6c.3 0 .5.1.8.2.2.1.6.4.6.8s-.3.7-.6.8c-.3.1-.5.1-.8.1-.3 0-.5 0-.8-.1-.2-.1-.5-.3-.6-.7 0-.4.3-.7.5-.8.4-.2.7-.3.9-.3zm0 .6c-.4 0-.8.3-.7.4-.1.2.3.5.7.5.2 0 .4-.1.5-.2.1-.1.2-.2.2-.3 0-.1 0-.2-.2-.3-.1-.1-.3-.1-.5-.1z"/>
-    <text transform="matrix(.9708 0 0 1 262.834 89.142)" class="st1 st3" style="font-family:'ArialMT'">Forks</text>
+  </g>
 </svg>
 
 `
