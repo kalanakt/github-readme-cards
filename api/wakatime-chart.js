f630b3bc9c9fc330f364ceb2f80851ea6066da38
kalanakt <e19198@eng.pdn.ac.lk>
2023-04-11 13:10:30 +0530

    Update wakatime-chart.js

--- api/wakatime-chart.js
+++ api/wakatime-chart.js
@@ -17,20 +17,21 @@ router.get('/', async (req, res) => {
     const response = await axios.get(`https://wakatime.com/share/@${username}/${api}.json`);
     const data = response.data.data;
 
-    let chartData = "";
-    let chartLabels = "";
+    // let chartData = "";
+    // let chartLabels = "";
 
-    data.forEach((item) => {
-      chartData += `${item.percent},`;
-      chartLabels += `${item.name}|`;
-    });
+    // data.forEach((item) => {
+    //   chartData += `${item.percent},`;
+    //   chartLabels += `${item.name}|`;
+    // });
 
-    const chartUrl = `https://quickchart.io/chart?c={type:'doughnut',data:{datasets:[{data:[${chartData.slice(0, -1)}],backgroundColor:['#3581ba','#375eab','#16ce40','#dc9658','#083fa1','#d62728','#9467bd','#e44b23','#cb171e','#8c564b','#f34b7d','#1f9aef','#f1e05a','#aec7e8']}],labels:['${chartLabels.slice(0, -1)}']}}`;
-    const chartResponse = await axios.get(chartUrl, { responseType: "arraybuffer" });
-    const chartBuffer = Buffer.from(chartResponse.data, "binary");
+    // const chartUrl = `https://quickchart.io/chart?c={type:'doughnut',data:{datasets:[{data:[${chartData.slice(0, -1)}],backgroundColor:['#3581ba','#375eab','#16ce40','#dc9658','#083fa1','#d62728','#9467bd','#e44b23','#cb171e','#8c564b','#f34b7d','#1f9aef','#f1e05a','#aec7e8']}],labels:['${chartLabels.slice(0, -1)}']}}`;
+    // const chartResponse = await axios.get(chartUrl, { responseType: "arraybuffer" });
+    // const chartBuffer = Buffer.from(chartResponse.data, "binary");
 
-    res.setHeader("Content-Type", "image/svg+xml");
-    res.send(chartBuffer);
+    // res.setHeader("Content-Type", "image/svg+xml");
+    // res.send(chartBuffer);
+    res.send(data)
   } catch (error) {
     res.status(500).send(`Error: ${error.message}`);
   }
