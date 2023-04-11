e3edc7e481ce8dadf2379dc690ca3305d9f99b42
kalanakt <e19198@eng.pdn.ac.lk>
2023-04-11 14:39:03 +0530

    Update wakatime-chart.js

--- api/wakatime-chart.js
+++ api/wakatime-chart.js
@@ -38,14 +38,13 @@ router.get('/', async (req, res) => {
       }
     }));
 
-    // const chartUrl = `https://quickchart.io/chart?c=${chartjson}`;
+    const chartUrl = `https://quickchart.io/chart?c=${chartjson}`;
 
-    // const chartResponse = await axios.get(chartUrl, { responseType: "arraybuffer" });
-    // const chartBuffer = Buffer.from(chartResponse.data, "binary");
+    const chartResponse = await axios.get(chartUrl, { responseType: "arraybuffer" });
+    const chartBuffer = Buffer.from(chartResponse.data, "binary");
 
-    // res.setHeader("Content-Type", "image/svg+xml");
-    // res.send(chartBuffer);
-    res.send(chartjson)
+    res.setHeader("Content-Type", "image/svg+xml");
+    res.send(chartBuffer);
   } catch (error) {
     res.status(500).send(`Error: ${error.message}`);
   }
