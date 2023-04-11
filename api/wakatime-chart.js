c5f8f1dd0c2253cc1f1b6d62125ce952e4a74f1b
kalanakt <e19198@eng.pdn.ac.lk>
2023-04-11 14:29:38 +0530

    ggapi removed

--- api/wakatime-chart.js
+++ api/wakatime-chart.js
@@ -4,11 +4,6 @@ const fs = require("fs");
 const app = express();
 const router = express.Router()
 
-// Load the Visualization API and the corechart package.
-const google = require('googleapis').google;
-const charts = google.visualization;
-google.charts.load('current', { 'packages': ['corechart'] });
-
 // middleware that is specific to this router
 router.use((req, res, next) => {
   console.log('Time: ', Date.now())
@@ -22,60 +17,35 @@ router.get('/', async (req, res) => {
     const response = await axios.get(`https://wakatime.com/share/@${username}/${api}.json`);
     const data = response.data.data;
 
-    const datagg = new charts.DataTable();
-    datagg.addColumn('string', 'Task');
-    datagg.addColumn('number', 'Hours per Day');
-    for (let i = 0; i < 5; i++) {
-      datagg.addRow([`Task ${i}`, Math.floor(Math.random() * 10) + 1]);
-    }
+    let chartData = "";
+    let chartLabels = "";
+    let chartColor = "";
 
-    // Set chart options
-    const options = {
-      'title': 'Daily Activities',
-      'width': 400,
-      'height': 300
-    };
+    data.forEach((item) => {
+      chartData += `${item.percent},`;
+      chartLabels += `${item.name},`;
+      chartColor += `${item.color},`;
+    });
 
-    // Instantiate and draw our chart, passing in some options.
-    const chart = new charts.PieChart(document.createElement('div'));
-    chart.draw(datagg, options);
+    const chartjson = encodeURIComponent(JSON.stringify({
+      type: 'doughnut',
+      data: {
+        datasets: [{
+          data: [chartData.slice(0, -1)],
+          backgroundColor: [chartColor.slice(0, -1)]
+        }],
+        labels: [chartLabels.slice(0, -1)]
+      }
+    }));
 
-    // Get the SVG string of the chart
-    const svgString = chart.getContainer().innerHTML;
+    // const chartUrl = `https://quickchart.io/chart?c=${chartjson}`;
 
-    // Send the SVG as a response
-    res.setHeader('Content-Type', 'image/svg+xml');
-    res.send(svgString);
+    // const chartResponse = await axios.get(chartUrl, { responseType: "arraybuffer" });
+    // const chartBuffer = Buffer.from(chartResponse.data, "binary");
 
-    // let chartData = "";
-    // let chartLabels = "";
-    // let chartColor = "";
-
-    // data.forEach((item) => {
-    //   chartData += `${item.percent},`;
-    //   chartLabels += `${item.name},`;
-    //   chartColor += `${item.color},`;
-    // });
-
-    // const chartjson = encodeURIComponent(JSON.stringify({
-    //   type: 'doughnut',
-    //   data: {
-    //     datasets: [{
-    //       data: [chartData.slice(0, -1)],
-    //       backgroundColor: [chartColor.slice(0, -1)]
-    //     }],
-    //     labels: [chartLabels.slice(0, -1)]
-    //   }
-    // }));
-
-    // // const chartUrl = `https://quickchart.io/chart?c=${chartjson}`;
-
-    // // const chartResponse = await axios.get(chartUrl, { responseType: "arraybuffer" });
-    // // const chartBuffer = Buffer.from(chartResponse.data, "binary");
-
-    // // res.setHeader("Content-Type", "image/svg+xml");
-    // // res.send(chartBuffer);
-    // res.send(chartjson)
+    // res.setHeader("Content-Type", "image/svg+xml");
+    // res.send(chartBuffer);
+    res.send(chartjson)
   } catch (error) {
     res.status(500).send(`Error: ${error.message}`);
   }
