acce868cbb84157b3c72ab6c89a0968177463121
kalanakt <e19198@eng.pdn.ac.lk>
2023-04-11 15:35:00 +0530

    Update wakatime-chart.js

--- api/wakatime-chart.js
+++ api/wakatime-chart.js
@@ -66,7 +66,7 @@ router.get('/', async (req, res) => {
       ${chartjson.data.labels.map((label, index) => `
         <text x="${centerX - chartRadius - 30}" y="${centerY - chartRadius + 30 * index}" font-size="14">${label}</text>
         <rect x="${centerX - chartRadius - 50}" y="${centerY - chartRadius + 30 * index - 10}" width="20" height="20" fill="${chartjson.data.datasets[0].backgroundColor[index]}" />
-        <text x="${centerX - chartRadius + 30}" y="${centerY - chartRadius + 30 * index}" font-size="14">${chartjson.data.datasets[0].data[index]}%</text>
+        <text x="${centerX - chartRadius + 30}" y="${centerY - chartRadius + 30 * index}" font-size="14" fill="#fff">${chartjson.data.datasets[0].data[index]}%</text>
         `).join("")}
     </svg>
     `
