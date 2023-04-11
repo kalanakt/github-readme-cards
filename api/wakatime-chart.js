9e2399026467b7a3fed6b29a5b27438b9842c51a
kalanakt <e19198@eng.pdn.ac.lk>
2023-04-11 15:02:31 +0530

    Update wakatime-chart.js

--- api/wakatime-chart.js
+++ api/wakatime-chart.js
@@ -44,34 +44,34 @@ router.get('/', async (req, res) => {
     const centerY = svgHeight / 2; // Y coordinate of the center of the chart
     const chartRadius = 200; // Radius of the chart
 
-    // const svg = `
-    // <svg width="${svgWidth}" height="${svgHeight}">
-    //   ${chartjson.data.datasets.data.map((item, index) => {
-    //   const startAngle = index === 0 ? 0 : chartjson.data.datasets[0].data.slice(0, index).reduce((a, b) => parseFloat(a) + parseFloat(b), 0) / 100 * Math.PI * 2;
-    //   const endAngle = chartjson.data.datasets[0].data.slice(0, index + 1).reduce((a, b) => parseFloat(a) + parseFloat(b), 0) / 100 * Math.PI * 2;
-    //   const color = chartjson.data.datasets[0].backgroundColor[index];
-    //   return `
-    //       <path
-    //         d="
-    //           M ${centerX},${centerY}
-    //           L ${centerX + chartRadius * Math.cos(startAngle)},${centerY + chartRadius * Math.sin(startAngle)}
-    //           A ${chartRadius},${chartRadius} 0 ${endAngle - startAngle > Math.PI ? 1 : 0},1 ${centerX + chartRadius * Math.cos(endAngle)},${centerY + chartRadius * Math.sin(endAngle)}
-    //           Z
-    //         "
-    //         fill="${color}"
-    //       ></path>
-    //     `;
-    // }).join("")}
-    //   ${chartjson.data.labels.map((label, index) => `
-    //     <text x="${centerX - chartRadius - 30}" y="${centerY - chartRadius + 30 * index}" font-size="14">${label}</text>
-    //     <rect x="${centerX - chartRadius - 50}" y="${centerY - chartRadius + 30 * index - 10}" width="20" height="20" fill="${chartjson.data.datasets[0].backgroundColor[index]}" />
-    //     <text x="${centerX - chartRadius + 30}" y="${centerY - chartRadius + 30 * index}" font-size="14">${chartjson.data.datasets[0].data[index]}%</text>
-    //     `).join("")}
-    //     <!-- add chart title -->
-    //     <text x="${centerX}" y="${centerY - chartRadius - 20}" font-size="20" text-anchor="middle">${title}</text>
-    // </svg>`;
+    const svg = `
+    <svg width="${svgWidth}" height="${svgHeight}">
+      ${chartjson.data.datasets.data.map((item, index) => {
+      const startAngle = index === 0 ? 0 : chartjson.data.datasets[0].data.slice(0, index).reduce((a, b) => parseFloat(a) + parseFloat(b), 0) / 100 * Math.PI * 2;
+      const endAngle = chartjson.data.datasets[0].data.slice(0, index + 1).reduce((a, b) => parseFloat(a) + parseFloat(b), 0) / 100 * Math.PI * 2;
+      const color = chartjson.data.datasets[0].backgroundColor[index];
+      return `
+          <path
+            d="
+              M ${centerX},${centerY}
+              L ${centerX + chartRadius * Math.cos(startAngle)},${centerY + chartRadius * Math.sin(startAngle)}
+              A ${chartRadius},${chartRadius} 0 ${endAngle - startAngle > Math.PI ? 1 : 0},1 ${centerX + chartRadius * Math.cos(endAngle)},${centerY + chartRadius * Math.sin(endAngle)}
+              Z
+            "
+            fill="${color}"
+          ></path>
+        `;
+    }).join("")}
+      ${chartjson.data.labels.map((label, index) => `
+        <text x="${centerX - chartRadius - 30}" y="${centerY - chartRadius + 30 * index}" font-size="14">${label}</text>
+        <rect x="${centerX - chartRadius - 50}" y="${centerY - chartRadius + 30 * index - 10}" width="20" height="20" fill="${chartjson.data.datasets[0].backgroundColor[index]}" />
+        <text x="${centerX - chartRadius + 30}" y="${centerY - chartRadius + 30 * index}" font-size="14">${chartjson.data.datasets[0].data[index]}%</text>
+        `).join("")}
+        <!-- add chart title -->
+        <text x="${centerX}" y="${centerY - chartRadius - 20}" font-size="20" text-anchor="middle">${title}</text>
+    </svg>`;
 
-    res.send(chartjson);
+    res.send(svg);
   } catch (error) {
     res.status(500).send(`Error: ${error.message}`);
   }
